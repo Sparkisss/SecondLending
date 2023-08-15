b4df3941115b3b85c3fd28a16206210b22e8e4ca
Igor Siamykin <sparkisisss@gmail.com>
2023-08-15 18:11:47 +0300

    15-08-23

--- js/script.js
+++ js/script.js
@@ -1,17 +1,18 @@
 
-document.querySelector('.burger').addEventListener('click', function() {
-  this.classList.toggle('active');
-  document.querySelector('nav').classList.toggle('open');
-  document.querySelector('.comment__wrapper-second').classList.toggle('opacity');
-})
-
-
-
+const menu = document.querySelectorAll('.burger');
 const items = document.querySelectorAll('.middle__top-element');
 const example = document.querySelectorAll('.middle__info');
 
+menu.forEach((men, index) => {
+  men.addEventListener('click', function() {
+    menu[index].classList.toggle('active');
+    document.querySelector('nav').classList.toggle('open');
+  document.querySelector('.comment__wrapper-second').classList.toggle('opacity');
+  });
+});
+
 items.forEach((item, index) => {
   item.addEventListener('click', function () {
-    
+    example[index].classList.toggle('open');
   });
 });
